da55a8eedd10d2af27db1321f1ee6651f6104511
Michael Bazos <micabazos@gmail.com>
2018-12-20 01:26:25 +0100

    feat: integrate new icons

---
+++ src/lib/feather/archive.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+
+@Component({
+  selector: 'i-archive',
+  styles: [`
+    :host {
+      display: inline-block;
+      width: 24px;
+      height: 24px;
+      fill: none;
+      stroke: currentColor;
+      stroke-width: 2px;
+      stroke-linecap: round;
+      stroke-linejoin: round;
+    }
+  `],
+  template: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="feather feather-archive">
+    <polyline points="21 8 21 21 3 21 3 8"></polyline><rect x="1" y="3" width="22" height="5"></rect><line x1="10" y1="12" x2="14" y2="12"></line>
+  </svg>`
+})
+export class IconArchiveComponent {}
+
+@NgModule({
+  declarations: [ IconArchiveComponent ],
+  exports: [ IconArchiveComponent ]
+})
+export class IconArchive {}
---
+++ src/lib/feather/trello.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+
+@Component({
+  selector: 'i-trello',
+  styles: [`
+    :host {
+      display: inline-block;
+      width: 24px;
+      height: 24px;
+      fill: none;
+      stroke: currentColor;
+      stroke-width: 2px;
+      stroke-linecap: round;
+      stroke-linejoin: round;
+    }
+  `],
+  template: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="feather feather-trello">
+    <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><rect x="7" y="7" width="3" height="9"></rect><rect x="14" y="7" width="3" height="5"></rect>
+  </svg>`
+})
+export class IconTrelloComponent {}
+
+@NgModule({
+  declarations: [ IconTrelloComponent ],
+  exports: [ IconTrelloComponent ]
+})
+export class IconTrello {}
